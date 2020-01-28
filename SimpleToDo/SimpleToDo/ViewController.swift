1068cee8b3bf08be50cc5d33e374ba8153fff57d
ismael <asmael-1987@hotmail.com>
2020-01-28 18:52:40 +0100

    add commit editingStyle func

--- SimpleToDo/SimpleToDo/ViewController.swift
+++ SimpleToDo/SimpleToDo/ViewController.swift
@@ -164,6 +164,16 @@ class ViewController: UIViewController, UITextFieldDelegate,UITableViewDelegate,
         
         return cell!    }
     
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            // handle delete (by removing the data from your array and updating the tableview)
+        }
+    }
+    
 
 }
 
